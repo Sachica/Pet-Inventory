9dc2584db580e9074b23e248de3763ff9b5ca6bd
ManuelCoronelAndrade <andrademanuelalejandroc@ufps.edu.co>
2022-05-11 20:01:09 -0500

    Inicio de sesion funcionando

--- pet_inventory/apps/users/models.py
+++ pet_inventory/apps/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.first_name
--- pet_inventory/apps/users/views.py
+++ pet_inventory/apps/users/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
-
+from django import views
+from .models import User
 # Create your views here.
+
+
+class index(views.View):
+
+    def get(self, request):
+        return render(request, 'users/index.html')
+    
+
+
+class iniciarSesion(views.View):
+    
+    def get(self, request):
+        return render(request, 'users/iniciarSesion.html')
+    
+    def post(self, request):
+        print(request.POST)
+        email = request.POST['txtEmail']
+        password = request.POST['txtPassword']
+        user = User.objects.filter(email=email, password=password).first()
+        print(user)
+
+        if user is None :
+            return render(request, 'users/index.html')
+        else :
+            
+             return render(request, 'pets/dashboard.html')
